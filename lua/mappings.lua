a054d91a4423f74809d458d99d6642c71eae61ed
frazrepo <frazvisuals@gmail.com>
2021-12-07 09:57:23 +0100

    Update mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -170,6 +170,7 @@ map('n', '<Leader>m', [[mmHmt:%s/<C-V><cr>//ge<cr>'tzt'm]]  , {noremap =false, s
 function _G.set_terminal_keymaps()
   local opts = {noremap = true}
   vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-p>', [[<C-R>*]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
@@ -183,6 +184,10 @@ vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 ------------------------------------------------------------
 -- Text-Objects
 ------------------------------------------------------------
+
+-- All Object Ex : yA
+-------------------
+map('o', 'A',':<c-u>normal! ggVG<cr>', default_opts)
 
 -- Line text-object
 -------------------
