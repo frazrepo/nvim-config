7ef7999ac712e4d2d5cf2b2ea8f8f3ec36d68de2
frazrepo <frazvisuals@gmail.com>
2021-12-14 17:27:54 +0100

    Update mappings

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -29,6 +29,7 @@ function M.setup()
             ['<C-d>'] = cmp.mapping.scroll_docs(-4),
             ['<C-f>'] = cmp.mapping.scroll_docs(4),
             ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-o>'] = cmp.mapping.complete(),
             ['<C-e>'] = cmp.mapping.close(),
             ['<CR>'] = cmp.mapping.confirm({
                 select = true
