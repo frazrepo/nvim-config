02de25901dc037bdd9808f22fc88e20762ab31ef
frazrepo <frazvisuals@gmail.com>
2021-12-17 14:11:22 +0100

    Update null-ls config

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -30,7 +30,8 @@ require("null-ls").setup({
   sources = lSsources,
   on_attach = function(client)
     if client.resolved_capabilities.document_formatting then
-      vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+            -- Todo when installing stylelua and prettier
+            -- vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
     end
   end,
 })
