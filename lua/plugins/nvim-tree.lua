7a2697ec476f03bae5629ac931499d2f84fc05cf
frazrepo <frazvisuals@gmail.com>
2022-05-22 09:46:58 +0200

    Upgrade to nvim 0.7

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -13,12 +13,8 @@
 --- See: `help NvimTree`
 local g = vim.g
 
-g.nvim_tree_gitignore = 1
-g.nvim_tree_quit_on_open = 0
-g.nvim_tree_indent_markers = 1
 g.nvim_tree_git_hl = 1
 g.nvim_tree_highlight_opened_files = 1
-g.nvim_tree_disable_window_picker = 1
 g.nvim_tree_respect_buf_cwd = 1
 g.nvim_tree_width_allow_resize  = 1
 g.nvim_tree_show_icons = {
@@ -33,7 +29,7 @@ g.nvim_tree_icons = {
 
 require('nvim-tree').setup {
     open_on_setup = false,
-    auto_close = true,
+    -- auto_close = true,
     view = {
         width = 32,
         auto_resize = true
