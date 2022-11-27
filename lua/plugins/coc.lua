c42dd557fc05773af4cc4138511f2559221664a2
frazrepo <frazvisuals@gmail.com>
2022-11-27 22:25:13 +0100

    Update comment

--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -17,6 +17,9 @@
 -- * Coc omnisharp
 -- * Coc snippets + honza snippets
 -- * Coc lua
+--
+-- Inspiration from
+-- https://github.com/albingroen/quick.nvim/blob/main/lua/coc-config.lua
 
 local exec = vim.api.nvim_exec 	        -- execute Vimscript
 
@@ -27,10 +30,12 @@ vim.api.nvim_set_keymap("n", "gd", "<Plug>(coc-definition)", {silent = true})
 vim.api.nvim_set_keymap("n", "K", ":call CocActionAsync('doHover')<CR>", {silent = true, noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>rn", "<Plug>(coc-rename)", {})
 vim.api.nvim_set_keymap("n", ",f", ":CocCommand prettier.formatFile<CR>", {noremap = true})
+
 vim.api.nvim_set_keymap("i", "<C-Space>", "coc#refresh()", { silent = true, expr = true })
 vim.api.nvim_set_keymap("i", "<TAB>", "pumvisible() ? '<C-n>' : '<TAB>'", {noremap = true, silent = true, expr = true})
 vim.api.nvim_set_keymap("i", "<S-TAB>", "pumvisible() ? '<C-p>' : '<C-h>'", {noremap = true, expr = true})
 vim.api.nvim_set_keymap("i", "<CR>", "pumvisible() ? coc#_select_confirm() : '<C-G>u<CR><C-R>=coc#on_enter()<CR>'", {silent = true, expr = true, noremap = true})
+
 
 -- Coc yank
 vim.api.nvim_set_keymap("n", "<leader>y",":<C-u>CocList -A --normal yank<cr>", {noremap = true, silent = true})
