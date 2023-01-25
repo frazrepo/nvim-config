8833c09739248d859d3b81ee577d19b3788be691
frazrepo <frazvisuals@gmail.com>
2023-01-25 21:40:28 +0100

    Update to lazy.nvim

--- init.lua
+++ init.lua
@@ -14,5 +14,6 @@ Debug : nvim --startuptime vim.log
 
 require('settings')
 require('mappings')
-require('colors')
-require('plugins/packer')
+-- require('colors')
+-- require('plugins/packer')
+require('plugins/lazy')
