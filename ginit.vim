2e4ed67774ece1a084a41692a92c8c274f8d7108
frazrepo <frazvisuals@gmail.com>
2023-02-12 15:45:20 +0100

    Update font to support icons in neovide

--- ginit.vim
+++
@@ -1,3 +0,0 @@
-" For nvim-qt
-" GuiFont! Fira\ Code:h13
-GuiFont! JetBrains\ Mono:h11
--- lua/ginit.lua
+++ lua/ginit.lua
@@ -1,5 +1,5 @@
 vim.cmd([[
-        set guifont=JetBrains\ Mono:h11
+        set guifont=FiraCode\ Nerd\ Font\ Mono:h12
         let g:neovide_cursor_animation_length=0.02
         let g:neovide_cursor_trail_length=0
         let g:neovide_remember_window_size = v:true
