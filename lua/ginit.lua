0af329d57fad6f9a2c67773c972f1a573c8f0770
frazrepo <frazvisuals@gmail.com>
2023-02-12 15:46:58 +0100

    Add ginit.lua for gui font (neovide)

---
+++ lua/ginit.lua
@@ -0,0 +1,10 @@
+vim.cmd([[
+        set guifont=FiraCode\ Nerd\ Font\ Mono:h12
+        let g:neovide_cursor_animation_length=0.02
+        let g:neovide_cursor_trail_length=0
+        let g:neovide_remember_window_size = v:true
+  ]]
+)
+
+-- not working yet
+-- set lines=600 columns=800
