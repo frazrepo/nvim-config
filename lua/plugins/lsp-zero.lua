cb3afca6d2b7d08c1b5b963be55628b37d8c1b75
frazrepo <frazvisuals@gmail.com>
2023-02-15 08:24:31 +0100

    Update to supertab mappings

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -38,7 +38,13 @@ lsp.ensure_installed({
 })
 
 -- https://github.com/VonHeikemen/lsp-zero.nvim/blob/main/lua/lsp-zero/nvim-cmp-setup.lua
+-- https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#luasnip
 -- cmp options
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 vim.opt.completeopt  = { 'menu', 'menuone', 'noselect'}
 
@@ -57,20 +63,20 @@ lsp.setup_nvim_cmp({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      local col = vim.fn.col('.') - 1
+    ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
-        cmp.select_next_item(select_opts)
-      elseif luasnip.jumpable(1) then
-        luasnip.jump(1)
-      elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        fallback()
-      else
-        -- cmp.mapping.confirm({ select = false})
+        cmp.select_next_item()
+      -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
+      -- they way you will only jump inside the snippet region
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
         cmp.complete()
+      else
+        fallback()
       end
-    end, {'i', 's'}),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
       elseif luasnip.jumpable(-1) then
@@ -78,7 +84,7 @@ lsp.setup_nvim_cmp({
       else
         fallback()
       end
-    end, { 'i', 's' }),
+    end, { "i", "s" }),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
     ['<C-d>'] = cmp.mapping.scroll_docs(4),
