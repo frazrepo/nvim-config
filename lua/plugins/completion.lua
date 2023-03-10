20d87174a3bed7b9cc448cb102d99dcb8a29c86e
frazrepo <frazvisuals@gmail.com>
2023-03-10 21:35:34 +0100

    Update

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -68,15 +68,15 @@ function M.setup()
             {
                 name = 'nvim_lsp',
                 priority = 1000
-            }, 
+            },
             {
                 name = 'luasnip',
                 priority = 750
-            }, 
+            },
             {
                 name = 'buffer',
                 priority = 500
-            }, 
+            },
             {
                 name = 'path',
                 priority = 250
