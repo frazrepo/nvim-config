3ff832648cd52d5d2f2ead3a4db5b526d776ff67
frazrepo <frazvisuals@gmail.com>
2023-03-11 14:14:08 +0100

    Update config cursorline

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -403,7 +403,23 @@ require("lazy").setup({
     },
 
     -- cursorline
-    { 'yamatsum/nvim-cursorline' },
+    {
+        'yamatsum/nvim-cursorline',
+        config = function ()
+            require('nvim-cursorline').setup {
+                cursorline = {
+                    enable = true,
+                    timeout = 1000,
+                    number = false,
+                },
+                cursorword = {
+                    enable = true,
+                    min_length = 3,
+                    hl = { underline = true },
+                }
+            }
+        end
+    },
 
     -- lualine
     {
