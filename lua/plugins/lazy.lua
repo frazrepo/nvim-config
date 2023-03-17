395db3989b960597cd95a4e57f74a91fe845e432
frazrepo <frazvisuals@gmail.com>
2023-03-17 11:48:24 +0100

    splitjoin conflict mapping

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -204,14 +204,14 @@ require("lazy").setup({
     },
 
     -- splitjoin
-    {
-        'Wansmer/treesj',
-        keys = { '<space>m', '<space>j', '<space>s' },
-        dependencies = { 'nvim-treesitter/nvim-treesitter' },
-        config = function()
-            require('treesj').setup({--[[ your config ]]})
-        end,
-    },
+    -- {
+    --     'Wansmer/treesj',
+    --     keys = { '<space>m', '<space>j', '<space>s' },
+    --     dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    --     config = function()
+    --         require('treesj').setup({--[[ your config ]]})
+    --     end,
+    -- },
 
     -----------------------------------------------------------
     -- Search Replace
