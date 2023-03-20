636cae6a9323165150cfbc19ca107f8ce961c575
frazrepo <frazvisuals@gmail.com>
2023-03-20 08:35:03 +0100

    Add custom config

--- lua/fzconfig.lua
+++ lua/fzconfig.lua
@@ -1,5 +1,24 @@
-local config = {}
+-- Adapted from mkdir nvim plugin
+-- https://github.com/jghauser/mkdir.nvim
+--  create missing dir on saving a file
+local fn = vim.fn
+function run_mkdir()
+  local dir = fn.expand('<afile>:p:h')
 
--- TODO
+  -- This handles URLs using netrw. See ':help netrw-transparent' for details.
+  if dir:find('%l+://') == 1 then
+    return
+  end
 
-return config
+  if fn.isdirectory(dir) == 0 then
+    fn.mkdir(dir, 'p')
+  end
+end
+
+-- autogroup MkdirRun
+local mygroup = vim.api.nvim_create_augroup('MkdirRun', { clear = false })
+vim.api.nvim_create_autocmd({ 'BufWritePre'}, {
+  pattern = '*',
+  group = mygroup,
+  command = 'lua run_mkdir()',
+})
