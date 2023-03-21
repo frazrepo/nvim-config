a2c4680fc305b4a7617ac75a7db2a97490d69877
frazrepo <frazvisuals@gmail.com>
2023-03-21 08:47:42 +0100

    Update ufo fold config

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -520,7 +520,22 @@ require("lazy").setup({
             require('ufo').setup(
               {
                 provider_selector = function(bufnr, filetype, buftype)
-                  return {"treesitter", "indent"}
+                  -- return {"treesitter", "indent"}
+                    local function handleFallbackException(bufnr, err, providerName)
+                      if type(err) == "string" and err:match "UfoFallbackException" then
+                        return require("ufo").getFolds(bufnr, providerName)
+                      else
+                        return require("promise").reject(err)
+                      end
+                    end
+
+                    return (filetype == "" or buftype == "nofile") and "indent" -- only use indent until a file is opened
+                      or function(bufnr)
+                        return require("ufo")
+                          .getFolds(bufnr, "lsp")
+                          :catch(function(err) return handleFallbackException(bufnr, err, "treesitter") end)
+                          :catch(function(err) return handleFallbackException(bufnr, err, "indent") end)
+                      end
                 end
               }
             )
