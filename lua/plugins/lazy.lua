509680ca47900337e5c3ea58f9e1a86a525beea8
frazrepo <frazvisuals@gmail.com>
2023-03-22 20:57:24 +0100

    Test neovim session manager + dressing

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -479,7 +479,20 @@ require("lazy").setup({
         vim.api.nvim_set_keymap("n",'<leader>k', ":lua require('auto-session').SaveSession(require('auto-session').get_root_dir() .. vim.fn.input('SessionName > ')) <CR>", {noremap = true, silent = true})
       end
     },
-
+    
+    -- Alternative neovim session manager
+    -- bug avec des splits ? : works after restart
+    -- Another bug: seems to not working correctly when switching between sessions
+    -- {
+    --     "shatur/neovim-session-manager",
+    --     config = function()
+    --         require('session_manager').setup({
+    --             autoload_mode = require('session_manager.config').AutoloadMode.Disabled,             })
+    --         vim.api.nvim_set_keymap("n", "<leader>s", "<Cmd>SessionManager load_session<CR>", {noremap = true, silent = true})
+    --     end,
+    --     dependencies = {'stevearc/dressing.nvim'}
+    -- },
+    
     -----------------------------------------------------------
     -- UI enhacements
     -----------------------------------------------------------
@@ -560,36 +573,36 @@ require("lazy").setup({
     -- nvim ufo
     -- see settings.lua for fold* options
     -- todo : use https://github.com/luukvbaal/statuscol.nvim but needs nvim > 0.9
-    {
-        'kevinhwang91/nvim-ufo',
-        config = function()
-            -- default mapping zR openAllFolds, zM closeAllFolds
-            require('ufo').setup(
-              {
-                provider_selector = function(bufnr, filetype, buftype)
-                  -- return {"treesitter", "indent"}
-                    local function handleFallbackException(bufnr, err, providerName)
-                      if type(err) == "string" and err:match "UfoFallbackException" then
-                        return require("ufo").getFolds(bufnr, providerName)
-                      else
-                        return require("promise").reject(err)
-                      end
-                    end
+    -- {
+    --     'kevinhwang91/nvim-ufo',
+    --     config = function()
+    --         -- default mapping zR openAllFolds, zM closeAllFolds
+    --         require('ufo').setup(
+    --           {
+    --             provider_selector = function(bufnr, filetype, buftype)
+    --               -- return {"treesitter", "indent"}
+    --                 local function handleFallbackException(bufnr, err, providerName)
+    --                   if type(err) == "string" and err:match "UfoFallbackException" then
+    --                     return require("ufo").getFolds(bufnr, providerName)
+    --                   else
+    --                     return require("promise").reject(err)
+    --                   end
+    --                 end
 
-                    return (filetype == "" or buftype == "nofile") and "indent" -- only use indent until a file is opened
-                      or function(bufnr)
-                        return require("ufo")
-                          .getFolds(bufnr, "lsp")
-                          :catch(function(err) return handleFallbackException(bufnr, err, "treesitter") end)
-                          :catch(function(err) return handleFallbackException(bufnr, err, "indent") end)
-                      end
-                end
-              }
-            )
-        end,
-        dependencies = 'kevinhwang91/promise-async'
+    --                 return (filetype == "" or buftype == "nofile") and "indent" -- only use indent until a file is opened
+    --                   or function(bufnr)
+    --                     return require("ufo")
+    --                       .getFolds(bufnr, "lsp")
+    --                       :catch(function(err) return handleFallbackException(bufnr, err, "treesitter") end)
+    --                       :catch(function(err) return handleFallbackException(bufnr, err, "indent") end)
+    --                   end
+    --             end
+    --           }
+    --         )
+    --     end,
+    --     dependencies = 'kevinhwang91/promise-async'
 
-    },
+    -- },
 
     -----------------------------------------------------------
     -- Terminal enhacements
