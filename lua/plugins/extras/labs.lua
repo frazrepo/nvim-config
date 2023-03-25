13475d96b620e39fc5c06a7df6733eb047434816
frazrepo <frazvisuals@gmail.com>
2023-03-25 13:22:27 +0100

    Update

--- lua/plugins/extras/labs.lua
+++ lua/plugins/extras/labs.lua
@@ -5,16 +5,17 @@ return {
     -- vim gtfo - open files in terminal/explorer
     {
         'justinmk/vim-gtfo'
-    }
+    },
+
     -- splitjoin
     -- mapping conflict with <leader>s , sql buffer
-    -- {
-    --     'Wansmer/treesj',
-    --     keys = { '<space>jm', '<space>jj', '<space>js' },
-    --     dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    --     config = function()
-    --         require('treesj').setup({--[[ your config ]]})
-    --     end,
-    -- },
+    {
+        'Wansmer/treesj',
+        keys = { '<space>jm', '<space>jj', '<space>js' },
+        dependencies = { 'nvim-treesitter/nvim-treesitter' },
+        config = function()
+            require('treesj').setup({--[[ your config ]]})
+        end,
+    },
 
 }
