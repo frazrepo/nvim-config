73ae81e345e8b2aaec75572da217b234d6bc56de
frazrepo <frazvisuals@gmail.com>
2023-03-25 13:36:47 +0100

    Update add splitjoin

--- lua/plugins/extras/labs.lua
+++ lua/plugins/extras/labs.lua
@@ -9,12 +9,19 @@ return {
 
     -- splitjoin
     -- mapping conflict with <leader>s , sql buffer
+    -- jk : toggle, jj : join, js : split
     {
         'Wansmer/treesj',
-        keys = { '<space>jm', '<space>jj', '<space>js' },
+        keys = { '<space>jk', '<space>jj', '<space>js' },
         dependencies = { 'nvim-treesitter/nvim-treesitter' },
         config = function()
-            require('treesj').setup({--[[ your config ]]})
+            require('treesj').setup({
+                use_default_keymaps = false,    
+            })
+            -- For use default preset and it work with dot
+            vim.keymap.set('n', '<leader>jk', require('treesj').toggle, {silent = false, desc = "SplitJoin Toggle"})
+            vim.keymap.set('n', '<leader>jj', require('treesj').join,{ desc = "SplitJoin Join"})
+            vim.keymap.set('n', '<leader>js', require('treesj').split, { desc = "SplitJoin Split"})
         end,
     },
 
