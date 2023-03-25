0c86bf96835afb4db1f9eb22e6f119a0c6c97d9a
frazrepo <frazvisuals@gmail.com>
2023-03-25 22:42:39 +0100

    Update leap mapping (conflict with surround)

--- lua/plugins/extras/motion.lua
+++ lua/plugins/extras/motion.lua
@@ -4,15 +4,23 @@ return {
   -----------------------------------------------------------
 
   -- leap like next version of lightspeed
+  -- s{char1}{char2} then jump to next /previous with Enter/Tab
+  -- operator mode (till) :  x/X{char1}{char2} : eg dxca (before ca)
+  -- operator mode (to) : replace s/S with z/Z : dzca (include)
+  -- x/X -->  z/Z : t/T --> f/F
+  -- visual mode : vx/X{char1}{char2}
   {
       'ggandor/leap.nvim',
       config = function()
-          require('leap').add_default_mappings()
+        require('leap').add_default_mappings()
+        -- Override operator pending mode (conflict with nvim-surround)
+        vim.keymap.set('o', 'z', '<Plug>(leap-forward-to)')
+        vim.keymap.set('o', 'Z', '<Plug>(leap-backward-to)')
       end,
   },
 
   -- Flit : easily jump to any location and enhanced f/t motions for Leap
-  -- Can jump on the next occurence with <CR>
+  -- Can jump/previous on the next occurence with <CR>/<Tab>
   {
       "ggandor/flit.nvim",
       dependencies ={ "ggandor/leap.nvim"},
