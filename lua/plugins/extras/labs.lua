404c0d544c9edbe2fcf5f57feb484862eebd68a0
frazrepo <frazvisuals@gmail.com>
2023-03-25 11:16:37 +0100

    Merge from labs - Refactor

--- lua/plugins/extras/labs.lua
+++ lua/plugins/extras/labs.lua
@@ -2,11 +2,15 @@
 -- if true then return {} end
 
 return {
+    -- vim gtfo - open files in terminal/explorer
+    {
+        'justinmk/vim-gtfo'
+    }
     -- splitjoin
     -- mapping conflict with <leader>s , sql buffer
     -- {
     --     'Wansmer/treesj',
-    --     keys = { '<space>m', '<space>j', '<space>s' },
+    --     keys = { '<space>jm', '<space>jj', '<space>js' },
     --     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     --     config = function()
     --         require('treesj').setup({--[[ your config ]]})
