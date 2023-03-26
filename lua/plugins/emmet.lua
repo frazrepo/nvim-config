d97d947046a1d05ee04c143b74c2eacfa905a521
frazrepo <frazvisuals@gmail.com>
2023-03-26 14:29:03 +0200

    Update emmet

--- lua/plugins/emmet.lua
+++ lua/plugins/emmet.lua
@@ -5,11 +5,9 @@ local M = {}
 
 M.setup = function()
     -- vim.g.user_emmet_leader_key = "<C-Z>"
-    vim.cmd [[
-        imap <C-e> <Plug>(emmet-expand-abbr)
-        nmap )e <Plug>(emmet-move-next)
-        nmap (e <Plug>(emmet-move-prev)
-    ]]
+    vim.keymap.set("i", "<C-e>", "<Plug>(emmet-expand-abbr)", {remap = true})
+    vim.keymap.set("n", ")e", "<Plug>(emmet-move-next)", {remap = true})
+    vim.keymap.set("n", "(e", "<Plug>(emmet-move-prev)", {remap = true})
 end
 
 return M
