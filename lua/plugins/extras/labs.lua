4a1d9b01aac2b128e59019ccf79dc62e26a2b1b9
Francis RAZANAMPARANY <RC6043@engie.com>
2025-01-03 18:15:46 +0100

    Add grug-far for search and conform for formatting

--- lua/plugins/extras/labs.lua
+++ lua/plugins/extras/labs.lua
@@ -3,6 +3,80 @@
 -- Moves config to extras.lua when validated
 
 return {
+    -- Added on 20250103
+    -- Formatter 
+    {
+        "stevearc/conform.nvim",
+        event = "BufReadPre",
+        config = function()
+            vim.g.autoformat = true
+            require("conform").setup({
+                formatters_by_ft = {
+                    bicep = { "bicep" },
+                    css = { "prettier" },
+                    go = { "goimports_reviser", "gofmt", "golines" },
+                    html = { "prettier" },
+                    javascript = { "prettier" },
+                    typescript = { "prettier" },
+                    json = { "prettier" },
+                    lua = { "stylua" },
+                    markdown = { "prettier" },
+                    nix = { "nixfmt" },
+                    rust = { "rustfmt" },
+                    scss = { "prettier" },
+                    sh = { "shfmt" },
+                    templ = { "templ" },
+                    toml = { "taplo" },
+                    yaml = { "prettier" },
+                },
+
+                format_after_save = function()
+                    if not vim.g.autoformat then
+                        return
+                    else
+                        if vim.bo.filetype == "ps1" then
+                            vim.lsp.buf.format()
+                            return
+                        end
+                        return { lsp_format = "fallback" }
+                    end
+                end,
+
+                formatters = {
+                    goimports_reviser = {
+                        command = "goimports-reviser",
+                        args = { "-output", "stdout", "$FILENAME" },
+                    },
+                },
+            })
+
+            -- Override bicep's default indent size
+            require("conform").formatters.bicep = {
+                args = { "format", "--stdout", "$FILENAME", "--indent-size", "4" },
+            }
+
+            -- Override prettier's default indent type
+            require("conform").formatters.prettier = {
+                prepend_args = { "--tab-width", "4" },
+            }
+        end,
+    },
+
+    -- Added on 20250103
+    -- grug-far Search and Replace
+    {
+        'MagicDuck/grug-far.nvim',
+        cmd = "GrugFar",
+        config = function()
+            require('grug-far').setup({
+                windowsCreationCommand="botright vsplit %"
+                -- options, see Configuration section below
+                -- there are no required options atm
+                -- engine = 'ripgrep' is default, but 'astgrep' can be specified
+            });
+        end
+    },
+
     -- splitjoin
     -- mapping conflict with <leader>s , sql buffer
     -- kk : toggle, kj : join, ks : split
--- lua/plugins/lsp/none-ls.lua
+++ lua/plugins/lsp/none-ls.lua
@@ -6,49 +6,49 @@
 
 -- Ensure some tools are installed
 require("mason-null-ls").setup({
-    ensure_installed = { "prettier", "stylua", "jq", "sqlformat" },
-    -- For the remaininf, just install with :NoneLsInstall tools
+	ensure_installed = { "prettier", "stylua", "jq", "sqlformat" },
+	-- For the remaining, just install with :NoneLsInstall tools
 })
 
 -- Formatting config
 -- Needs to install each binary associated with each formatter : prettier, sqlformat, sqllua, ...
 local null_ls = require("null-ls")
 local sources = {
-    null_ls.builtins.formatting.prettier.with({
-        filetypes = {
-            "javascript",
-            "typescript",
-            "css",
-            "scss",
-            "html",
-            "json",
-            "yaml",
-            "markdown",
-            "graphql",
-            "md",
-            "txt",
-        },
-    }),
-    null_ls.builtins.formatting.stylua.with({
-        args = { "--indent-width", "2", "--indent-type", "Spaces", "-" },
-    }),
-    null_ls.builtins.formatting.sql_formatter
+	null_ls.builtins.formatting.prettier.with({
+		filetypes = {
+			"javascript",
+			"typescript",
+			"css",
+			"scss",
+			"html",
+			"json",
+			"yaml",
+			"markdown",
+			"graphql",
+			"md",
+			"txt",
+		},
+	}),
+	null_ls.builtins.formatting.stylua.with({
+		args = { "--indent-width", "2", "--indent-type", "Spaces", "-" },
+	}),
+	null_ls.builtins.formatting.sql_formatter,
 }
 
 local map = vim.api.nvim_set_keymap
 require("null-ls").setup({
-    sources = sources,
-    on_attach = function(client)
-        map("n", ",f", "<cmd>lua vim.lsp.buf.format { async = true }<CR>", { noremap = true, silent = true })
+	sources = sources,
+	on_attach = function(client)
+		map("n", ",f", "<cmd>lua vim.lsp.buf.format { async = true }<CR>", { noremap = true, silent = true })
 
-        -- Create a command `:Format` local to the LSP buffer
-        vim.api.nvim_create_user_command("Format", function()
-            vim.lsp.buf.format({ async = true })
-        end, { desc = "Format current buffer with LSP" })
+		-- Create a command `:Format` local to the LSP buffer
+		vim.api.nvim_create_user_command("Format", function()
+			vim.lsp.buf.format({ async = true })
+		end, { desc = "Format current buffer with LSP" })
 
-        if client.server_capabilities.document_formatting then
-            -- Todo when installing stylelua, sqlformat and prettier
-            -- vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-        end
-    end,
+		if client.server_capabilities.document_formatting then
+			-- Todo when installing stylelua, sqlformat and prettier
+			-- vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+		end
+	end,
 })
