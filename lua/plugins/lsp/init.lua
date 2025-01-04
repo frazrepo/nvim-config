78a0def24738c881fa640daebd343fd878e64673
frazrepo <frazvisuals@gmail.com>
2025-01-04 22:01:37 +0100

    Update lsp capabilities

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -106,8 +106,7 @@ return {
 			--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
 			--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			-- capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-			capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
+			capabilities = vim.tbl_deep_extend("force", capabilities, require("blink.cmp").get_lsp_capabilities(capabilities))
 
 			-- Enable the following language servers
 			--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -170,7 +169,6 @@ return {
 					"tailwindcss",
 					"svelte",
 					"lua_ls",
-					"graphql",
 					"emmet_ls",
 					"prismals",
 					"pyright",
