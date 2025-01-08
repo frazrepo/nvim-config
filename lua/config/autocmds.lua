f173aebe4f18b09effcafc137d55bc1bb18b9473
frazrepo <frazvisuals@gmail.com>
2025-01-08 22:53:37 +0100

    Convert to lua

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,16 @@
--- Adapted from mkdir nvim plugin
--- https://github.com/jghauser/mkdir.nvim
---  create missing dir on saving a file
+
+-- autogroup TextYankPost : highlight yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+    pattern = "*",
+    callback = function()
+        vim.highlight.on_yank { higroup = "IncSearch", timeout = 150 }
+    end,
+})
+
+
+-- autogroup MkdirRun
+-- Adapted from mkdir nvim plugin : https://github.com/jghauser/mkdir.nvim
+-- Create missing dir on saving a file
 local fn = vim.fn
 function run_mkdir()
   local dir = fn.expand('<afile>:p:h')
@@ -15,7 +25,6 @@ function run_mkdir()
   end
 end
 
--- autogroup MkdirRun
 local mygroup = vim.api.nvim_create_augroup('MkdirRun', { clear = false })
 vim.api.nvim_create_autocmd({ 'BufWritePre'}, {
   pattern = '*',
@@ -24,45 +33,114 @@ vim.api.nvim_create_autocmd({ 'BufWritePre'}, {
 })
 
 
-local bo = vim.bo
-local getCursor = vim.api.nvim_win_get_cursor
-local setCursor = vim.api.nvim_win_set_cursor
 
--- local M = {}
-local keymap = vim.keymap.set
--- https://github.com/chrisgrieser/nvim-various-textobjs/blob/main/lua/various-textobjs.lua
+-- Helper Functions
+vim.api.nvim_exec([[
+        function! CleanExtraSpaces()
+            let save_cursor = getpos(".")
+            let old_query = getreg('/')
+            silent! %s/\s\+$//e
+            call setpos('.', save_cursor)
+            call setreg('/', old_query)
+        endfun
 
+        function! CmdLine(str)
+            call feedkeys(":" . a:str)
+        endfunction
 
----@return boolean
-local function isVisualLineMode()
-	local modeWithV = fn.mode():find("V")
-	return modeWithV ~= nil
-end
+        function! VisualSelection(direction, extra_filter) range
+            let l:saved_reg = @"
+            execute "normal! vgvy"
 
----runs :normal natively with bang
----@param cmdStr any
-local function normal(cmdStr)
-	local is08orHigher = vim.version().major > 0 or vim.version().minor > 7
-	if is08orHigher then
-		vim.cmd.normal { cmdStr, bang = true }
-	else
-		vim.cmd("normal! " .. cmdStr)
-	end
-end
+            let l:pattern = escape(@", "\\/.*'$^~[]")
+            let l:pattern = substitute(l:pattern, "\n$", "", "")
 
----sets the selection for the textobj (linewise)
----@param startline integer
----@param endline integer
--- local function setLinewiseSelection(startline, endline)
-function setLinewiseSelection(startline, endline)
-	setCursor(0, { startline, 0 })
-	if not isVisualLineMode() then normal("V") end
-	normal("o")
-	setCursor(0, { endline, 0 })
-end
+            if a:direction == 'gv'
+                call CmdLine("Ack '" . l:pattern . "' " )
+            elseif a:direction == 'replace'
+                call CmdLine("%s" . '/'. l:pattern . '/')
+            endif
 
+            let @/ = l:pattern
+            let @" = l:saved_reg
+        endfunction
 
--- Experimental mapping ygG to select entire buffer
--- meme résultat que yie, on ne garder pas la position du curseur
+        function! QuickFixToggle()
+            if empty(filter(getwininfo(), 'v:val.quickfix'))
+                copen
+            else
+                cclose
+            endif
+        endfunction
 
-keymap({ "o", "x" }, "gG", function()  setLinewiseSelection(1, fn.line("$")) end, { desc = "entireBuffer" .. " textobj" })
+        function! ExecuteMacroOverVisualRange()
+          echo "@".getcmdline()
+          execute ":'<,'>normal @".nr2char(getchar())
+        endfunction
+
+        function! SortLinesByWidth() range
+            silent! execute a:firstline . "," . a:lastline . 's/^\(.*\)$/\=strdisplaywidth( submatch(0) ) . " " . submatch(0)/'
+            silent! execute a:firstline . "," . a:lastline . 'sort n'
+            silent! execute a:firstline . "," . a:lastline . 's/^\d\+\s//'
+        endfunction
+
+    ]], false)
+
+-- Commands
+
+--  VisualBlock :  Workaround to start visual block mode on terminal if C-v or C-q is not working
+vim.cmd(
+    [[
+    command! VisualBlock execute "normal! \<C-v>"
+    ]]
+)
+
+--  W : sudo saves the file (useful for handling the permission-denied error on Linux)
+vim.cmd(
+    [[
+    command! W w !sudo tee % > /dev/null
+    ]]
+)
+
+-- SortByWidth : Sort lines by width
+vim.cmd(
+    [[
+    command! -range=%  SortByWidth <line1>,<line2>call SortLinesByWidth()
+    ]]
+)
+
+-- WipeReg : Clean all registers
+vim.cmd(
+    [[
+    command! WipeReg for i in range(34,122) | silent! call setreg(nr2char(i), []) | endfor
+    ]]
+)
+
+-- RemoveTrailingSpaces : Remove all training spaces
+vim.cmd(
+    [[
+    command! RemoveTrailingSpaces call CleanExtraSpaces()
+    ]]
+)
+
+-- AutoCommands
+vim.api.nvim_exec([[
+    augroup AutoCommandsGroup
+        autocmd!
+
+        " Clean extra spaces on txt files
+        autocmd BufWritePre *.txt,*.js,*.py,*.wiki,*.sh,*.coffee :call CleanExtraSpaces()
+
+        " Help File speedups, <enter> to follow tag, delete (backspace) for back
+        autocmd filetype help nnoremap <buffer><cr> <c-]>
+        autocmd filetype help nnoremap <buffer><bs> <c-T>
+        autocmd filetype help nnoremap <buffer>q :q<CR>
+        autocmd filetype help set nonumber
+        autocmd filetype help wincmd _ " Maximize the help on open
+
+        " AutoSave Scratch buffer
+        autocmd InsertLeave,TextChanged buffer.* nested silent! update
+        autocmd FocusGained,BufEnter,CursorHold buffer.* silent! checktime
+
+    augroup END
+ ]], false)
