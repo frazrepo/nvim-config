a2fe16927957ffff8550711d38348fc0bf35c3f7
frazrepo <frazvisuals@gmail.com>
2025-01-09 10:09:57 +0100

    Remove disabled status for neoformat plugin in configuration

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -5,6 +5,6 @@
 return {
     -- To disabled plugin , put a line with enabled = false for each plugin
     -- { "stevearc/conform.nvim", enabled = false }
-    { 'sbdchd/neoformat', enabled = false },
+    -- { 'sbdchd/neoformat', enabled = false },
 
 }
