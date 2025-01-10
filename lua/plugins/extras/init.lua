13401ac065716d926601a02daacc055e13f71cae
frazrepo <frazvisuals@gmail.com>
2025-01-10 01:00:03 +0100

    Add dropbar plugin

--- lua/plugins/extras/init.lua
+++ lua/plugins/extras/init.lua
@@ -3,6 +3,8 @@
 return {
     { import = 'plugins.extras.motion'},
     { import = 'plugins.extras.util'},
-    -- Validated plugins should be move to specific folder
+    { import = 'plugins.extras.ui'},
+
+    -- Validated plugins in extras should be move to specific folder
     { import = 'plugins.extras.lab'},
 }
