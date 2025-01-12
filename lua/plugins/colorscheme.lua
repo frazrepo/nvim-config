ead1db1ef923d03acd98405cef6dbc24279cc463
frazrepo <frazvisuals@gmail.com>
2025-01-12 10:49:40 +0100

    Update colorscheme configuration to enable Tokyo Night and disable Catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,16 +8,17 @@ return {
         priority = 1000, -- make sure to load this before all the other start plugins
         config = function()
             -- load the colorscheme here
-            -- vim.cmd.colorscheme("tokyonight")
+            vim.cmd.colorscheme("tokyonight")
         end,
     },
     {
         "catppuccin/nvim",
         lazy = false,
+        enable = false,
         priority = 1000,
         config = function()
             -- load the colorscheme here
-            vim.cmd.colorscheme("catppuccin")
+            --vim.cmd.colorscheme("catppuccin")
         end,
     },
 }
