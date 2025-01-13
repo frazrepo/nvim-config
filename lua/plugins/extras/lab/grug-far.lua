bb4e32a7fea53143d543c6f1a5c47cc0cb389dba
frazrepo <frazvisuals@gmail.com>
2025-01-13 18:03:37 +0100

    Update grug-far options

--- lua/plugins/extras/lab/grug-far.lua
+++ lua/plugins/extras/lab/grug-far.lua
@@ -2,15 +2,34 @@
 return {
 	-- grug-far Search and Replace
 	{
-		"MagicDuck/grug-far.nvim",
-		cmd = "GrugFar",
-		config = function()
-			require("grug-far").setup({
-				windowsCreationCommand = "botright vsplit %",
-				-- options, see Configuration section below
-				-- there are no required options atm
-				-- engine = 'ripgrep' is default, but 'astgrep' can be specified
-			})
-		end,
+        "MagicDuck/grug-far.nvim",
+        opts = {
+            headerMaxWidth = 80,
+        },
+        cmd = "GrugFar",
+        keys = {
+            {
+                --ripgrep not working yet on windows
+                "<leader>sr",
+                function()
+                    local grug = require("grug-far")
+                    local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+                    grug.open({
+                        transient = true,
+                        prefills = {
+                            filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+                        },
+                    })
+                end,
+                mode = { "n", "v" },
+                desc = "Search and Replace",
+            },
+            {
+                "<leader>sa",
+                "<cmd> lua require('grug-far').open({ engine = 'astgrep' })<cr>",
+                mode = { "n", "v" },
+                desc = "Search and Replace",
+            },
+        },
 	},
 }
