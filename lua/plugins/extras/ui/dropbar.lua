e0c79feb6cc8adb37d88fb4da7283fd43a0e289f
frazrepo <frazvisuals@gmail.com>
2025-01-16 22:47:25 +0100

    Update dropbar mappings

--- lua/plugins/extras/ui/dropbar.lua
+++ lua/plugins/extras/ui/dropbar.lua
@@ -9,7 +9,8 @@ return {
         },
         config = function()
           local dropbar_api = require('dropbar.api')
-          vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+          -- Disable this mappings as it conflicts with livegrep for telescope
+          -- vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
           vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
           vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
         end
