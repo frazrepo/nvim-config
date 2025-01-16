f7f40592392296324e61f9449387fe1f5ff41b24
frazrepo <frazvisuals@gmail.com>
2025-01-16 18:41:15 +0100

    Fix colorscheme to catpuccin as neotree won't follow file using tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,6 +6,7 @@ return {
         "folke/tokyonight.nvim",
         lazy = false, -- make sure we load this during startup if it is your main colorscheme
         priority = 1000, -- make sure to load this before all the other start plugins
+        enable = false,
         config = function()
             -- load the colorscheme here
             vim.cmd.colorscheme("tokyonight")
@@ -14,11 +15,11 @@ return {
     {
         "catppuccin/nvim",
         lazy = false,
-        enable = false,
+        -- enable = false,
         priority = 1000,
         config = function()
             -- load the colorscheme here
-            --vim.cmd.colorscheme("catppuccin")
+            vim.cmd.colorscheme("catppuccin")
         end,
     },
 }
