23274756575e4d086ddd6fa428426d2c0baf697c
frazrepo <frazvisuals@gmail.com>
2025-01-19 22:01:33 +0100

    Add mapping for mini files

--- lua/plugins/extras/lab/minifiles.lua
+++ lua/plugins/extras/lab/minifiles.lua
@@ -3,6 +3,21 @@ return
     {
         'echasnovski/mini.files',
         version = '*',
-        opts = {}
-    },
-}
+        opts = {},
+        keys = {
+            {
+                "<leader>fm",
+                function()
+                    require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+                end,
+                desc = "Open mini.files (Directory of Current File)",
+            },
+            {
+                "<leader>fM",
+                function()
+                    require("mini.files").open(vim.uv.cwd(), true)
+                end,
+                desc = "Open mini.files (cwd)",
+            },
+        },    },
+    }
