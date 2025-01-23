87da79b8ce6b5beb1f5ae1b331eddf4bec1d625f
frazrepo <frazvisuals@gmail.com>
2025-01-23 17:42:25 +0100

    Refactoring

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -37,7 +37,7 @@ return {
         }
     },
     -----------------------------------------------------------
-    -- File explorer  neo-tree
+    -- File explorer
     -----------------------------------------------------------
     {
         "nvim-neo-tree/neo-tree.nvim",
@@ -70,6 +70,32 @@ return {
             map('n', '<C-n>', ':Neotree action=focus toggle=true<CR>', default_opts)
         end,
     },
+    -- mini.files
+    -- Usage : navigate using hjkl
+    -- Manipalute file/dir creation like a normal buffer
+    -- confirm operation with =
+    -- Close dialog with q
+    {
+        'echasnovski/mini.files',
+        version = '*',
+        opts = {},
+        keys = {
+            {
+                "-",
+                function()
+                    require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+                end,
+                desc = "Open mini.files (Directory of Current File)",
+            },
+            {
+                "<leader>fM",
+                function()
+                    require("mini.files").open(vim.uv.cwd(), true)
+                end,
+                desc = "Open mini.files (cwd)",
+            },
+        },
+    },    
     
     -----------------------------------------------------------
     -- Better Quickfix vim-bqf
---
+++ lua/plugins/motion.lua
@@ -0,0 +1,45 @@
+    -----------------------------------------------------------
+    -- Motions
+    -----------------------------------------------------------
+    return {
+        -- leap like next version of lightspeed
+        -- s{char1}{char2} then jump to next /previous with Enter/Tab
+        -- operator mode (till) :  x/X{char1}{char2} : eg dxca (before ca)
+        -- operator mode (to) : replace s/S with z/Z : dzca (include)
+        -- x/X -->  z/Z : t/T --> f/F
+        -- visual mode : vx/X{char1}{char2} , vz/Z (vS not working, taken by surround)
+        -- s space space : blank line
+        -- s char space: char + end of line
+        {
+            'ggandor/leap.nvim',
+            config = function()
+                -- require('leap').add_default_mappings()
+                -- Override operator pending mode (conflict with nvim-surround)
+                vim.keymap.set({'n'}, 's', '<Plug>(leap-forward-to)')
+                vim.keymap.set({'n'}, 'S', '<Plug>(leap-backward-to)')
+                vim.keymap.set({'x', 'o'}, 'x', '<Plug>(leap-forward-till)')
+                vim.keymap.set({'x', 'o'}, 'X', '<Plug>(leap-backward-till)')
+                vim.keymap.set({'x', 'o'}, 'z', '<Plug>(leap-forward-to)')
+                vim.keymap.set({'x', 'o'}, 'Z', '<Plug>(leap-backward-to)')
+            end,
+        },    
+        
+        -- Flit : easily jump to any location and enhanced f/t motions for Leap
+        -- Can jump/previous on the next occurence with <CR>/<Tab>
+        {
+            "ggandor/flit.nvim",
+            dependencies ={ "ggandor/leap.nvim"},
+            event = "VeryLazy",
+            config = function(_, opts)
+                require('flit').setup {
+                    keys = { f = 'f', F = 'F', t = 't', T = 'T' },
+                    -- A string like "nv", "nvo", "o", etc.
+                    labeled_modes = "v",
+                    multiline = true,
+                    -- Like `leap`s similar argument (call-specific overrides).
+                    -- E.g.: opts = { equivalence_classes = {} }
+                    opts = {}
+                }
+            end,
+        }
+}
