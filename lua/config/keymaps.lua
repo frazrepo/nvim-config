42bd2c95f879b5b5a1661d2a29ac1a136fef2bf9
frazrepo <frazvisuals@gmail.com>
2025-01-24 21:03:23 +0100

    Clean unused object

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,7 +10,6 @@ local utils = require('config.utils')
 
 local map = vim.api.nvim_set_keymap
 local default_opts = { noremap = true, silent = true  }
-local cmd = vim.cmd
 
 -- Leader space
 vim.g.mapleader = " "
