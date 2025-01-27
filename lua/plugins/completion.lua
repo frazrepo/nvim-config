07d5334a14a40844fc5b2e364a021376c4fc77c6
frazrepo <frazvisuals@gmail.com>
2025-01-27 09:39:03 +0100

    Update setting for blink

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -43,6 +43,9 @@ return {
 					auto_show = true,
 					auto_show_delay_ms = 200,
 				  },
+				trigger = { 
+					show_on_insert_on_trigger_character = false,
+				}
 			}
 		},
 		opts_extend = { "sources.default" },
