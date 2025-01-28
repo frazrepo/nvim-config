1a003bd9d2386fcf386c61c826e1f29e24796711
frazrepo <frazvisuals@gmail.com>
2025-01-28 15:52:39 +0100

    Enhance NeoTree configuration with additional mappings for Git and Buffer explorers

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -30,6 +30,7 @@ return {
 
     -----------------------------------------------------------
     -- File explorer
+    -- Reference from https://github.com/LazyVim/LazyVim/blob/132986a624b49bf740161d90ce94f16dd5ea5883/lua/lazyvim/plugins/editor.lua#L5
     -----------------------------------------------------------
     {
         "nvim-neo-tree/neo-tree.nvim",
@@ -49,19 +50,78 @@ return {
                   require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
                 end,
                 desc = "Explorer NeoTree (cwd)",
+            },            
+            {
+                "<leader>ge",
+                function()
+                  require("neo-tree.command").execute({ source = "git_status", toggle = true })
+                end,
+                desc = "Git Explorer",
+              },
+              {
+                "<leader>be",
+                function()
+                  require("neo-tree.command").execute({ source = "buffers", toggle = true })
+                end,
+                desc = "Buffer Explorer",
               },            
         },
-        config = function()
-            require("neo-tree").setup({
-                filesystem = {
-                    follow_current_file = {
-                        enabled = true,
-                    } ,
-                    use_libuv_file_watcher = true,
-                }
+        opts = {
+            sources = { "filesystem", "buffers", "git_status" },
+            open_files_do_not_replace_types = { "terminal","qf"},
+            filesystem = {
+                bind_to_cwd = false,
+                follow_current_file = { enabled = true },
+                use_libuv_file_watcher = true,
+              },
+            window = {
+                mappings = {
+                    ["l"] = "open",
+                    ["h"] = "close_node",
+                    ["<space>"] = "none",
+                    ["Y"] = {
+                    function(state)
+                        local node = state.tree:get_node()
+                        local path = node:get_id()
+                        vim.fn.setreg("+", path, "c")
+                    end,
+                    desc = "Copy Path to Clipboard",
+                    },
+                    -- ["O"] = {
+                    -- function(state)
+                    --     require("lazy.util").open(state.tree:get_node().path, { system = true })
+                    -- end,
+                    -- desc = "Open with System Application",
+                    },
+                    ["P"] = { "toggle_preview", config = { use_float = false } },
+                },
+            default_component_configs = {
+                indent = {
+                    with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+                    expander_collapsed = "",
+                    expander_expanded = "",
+                    expander_highlight = "NeoTreeExpander",
+                },
+                git_status = {
+                    symbols = {
+                    unstaged = "󰄱",
+                    staged = "󰱒",
+                    },
+                },
+            },                
+        },
+        config = function(_,opts)              
+            require("neo-tree").setup(opts)
+            vim.api.nvim_create_autocmd("TermClose", {
+              pattern = "*lazygit",
+              callback = function()
+                if package.loaded["neo-tree.sources.git_status"] then
+                  require("neo-tree.sources.git_status").refresh()
+                end
+              end,
             })
         end,
-    },
+     },
     -- mini.files
     -- Usage : navigate using hjkl
     -- Manipalute file/dir creation like a normal buffer
