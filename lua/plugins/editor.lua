072aad66a1e2e70c45f47bd312f7abc5b4e8174b
frazrepo <frazvisuals@gmail.com>
2025-01-28 09:08:18 +0100

    Update which-key layout

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -161,6 +161,7 @@ return {
           -- your configuration comes here
           -- or leave it empty to use the default settings
           -- refer to the configuration section below
+          preset = "helix",
         },
         keys = {
           {
