a8d56c93a72455b81adfe47dd26ea4e46123a0ab
frazrepo <frazvisuals@gmail.com>
2025-01-28 09:03:47 +0100

    Update which-key settings

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -156,13 +156,21 @@ return {
     -----------------------------------------------------------
     {
         "folke/which-key.nvim",
-        config = function()
-            require("which-key").setup({
-                -- your configuration comes here
-                -- or leave it empty to the default settings
-                -- refer to the configuration section below
-            })
-        end,
+        event = "VeryLazy",
+        opts = {
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          -- refer to the configuration section below
+        },
+        keys = {
+          {
+            "<leader>?",
+            function()
+              require("which-key").show({ global = false })
+            end,
+            desc = "Buffer Local Keymaps (which-key)",
+          },
+        },
     },
 
     -----------------------------------------------------------
