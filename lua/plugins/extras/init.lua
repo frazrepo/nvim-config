fcfc43876a9e20c5e9f1c0e7419749cc823c6594
frazrepo <frazvisuals@gmail.com>
2025-01-29 15:04:27 +0100

    Add dap plugins

--- lua/plugins/extras/init.lua
+++ lua/plugins/extras/init.lua
@@ -4,6 +4,7 @@ return {
     { import = 'plugins.extras.util'},
     { import = 'plugins.extras.ui'},
     { import = 'plugins.extras.ai'},
+    { import = 'plugins.extras.dap'},
 
     -- Validated plugins in extras should be move to specific folder
     { import = 'plugins.extras.lab'},
