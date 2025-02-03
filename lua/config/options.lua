6be1fecf414dd86c20215ac5ea14e0a09ef7370a
frazrepo <frazvisuals@gmail.com>
2025-02-03 21:32:45 +0100

    Fix fold configuration

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -17,7 +17,7 @@ opt.expandtab               = true                          -- Use spaces instea
 
 opt.foldlevel               = 99
 opt.foldlevelstart          = 99
-opt.foldcolumn              = '1'
+opt.foldcolumn              = '5'
 opt.foldenable              = true
 
 opt.fillchars               = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
