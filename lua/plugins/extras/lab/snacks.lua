5b8a9a57a70c5e470a013c9dec6dcff1c8740de0
frazrepo <frazvisuals@gmail.com>
2025-02-03 19:37:16 +0100

    Fix mapping for autocmds in Snacks picker

--- lua/plugins/extras/lab/snacks.lua
+++ lua/plugins/extras/lab/snacks.lua
@@ -41,7 +41,7 @@ return {
             -- Additional search
             -- search
             { '<leader>s"', function() Snacks.picker.registers() end, desc = "Registers" },
-            -- { "<leader>sa", function() Snacks.picker.autocmds() end, desc = "Autocmds" },
+            { "<leader>sa", function() Snacks.picker.autocmds() end, desc = "Autocmds" },
             { "<leader>sc", function() Snacks.picker.command_history() end, desc = "Command History" },
             { "<leader>sC", function() Snacks.picker.commands() end, desc = "Commands" },
             { "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
