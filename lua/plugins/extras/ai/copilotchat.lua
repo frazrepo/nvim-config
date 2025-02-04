3b024ba3c5b556aa69465a5330a87249cfd3734f
frazrepo <frazvisuals@gmail.com>
2025-02-04 20:18:21 +0100

    Add mappings for copilotchat

--- lua/plugins/extras/ai/copilotchat.lua
+++ lua/plugins/extras/ai/copilotchat.lua
@@ -2,7 +2,6 @@ return {
     -- copilot chat
     {
         "CopilotC-Nvim/CopilotChat.nvim",
-        -- enabled = false,
         lazy = true,
         cond = FrazVim.extras.ai.engine == "copilot",
         dependencies = {
@@ -11,6 +10,39 @@ return {
             -- { "zbirenbaum/copilot.lua" , cond = FrazVim.extras.aiengine == "copilot" }, -- or github/copilot.vim
             { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
         },
+        keys = {
+            { "<leader>a", "", desc = "+ai", mode = { "n", "v" } },
+            {
+                "<leader>aa",
+                function()
+                    return require("CopilotChat").toggle()
+                end,
+                desc = "Toggle (CopilotChat)",
+                mode = { "n", "v" },
+            },
+            {
+                "<leader>ax",
+                function()
+                    return require("CopilotChat").reset()
+                end,
+                desc = "Clear (CopilotChat)",
+                mode = { "n", "v" },
+            },
+            {
+                "<leader>aq",
+                function()
+                    local input = vim.fn.input("Quick Chat: ")
+                    if input ~= "" then
+                        require("CopilotChat").ask(input)
+                    end
+                end,
+                desc = "Quick Chat (CopilotChat)",
+                mode = { "n", "v" },
+            },
+            -- Show prompts actions with telescope
+            -- { "<leader>ap", M.pick("prompt"), desc = "Prompt Actions (CopilotChat)", mode = { "n", "v" } },
+        },
+
         -- build = "make tiktoken", -- Only on MacOS or Linux
         opts = {
         -- See Configuration section for options
