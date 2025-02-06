598adc48faa6800297b3ccc3a031416c55e0305c
frazrepo <frazvisuals@gmail.com>
2025-02-06 09:14:11 +0100

    Update neogit configuration

--- lua/plugins/extras/lab/neogit.lua
+++ lua/plugins/extras/lab/neogit.lua
@@ -1,16 +1,16 @@
 return {
-    {
-        "NeogitOrg/neogit",
-        -- lazy = true,
-        dependencies = {
-          "nvim-lua/plenary.nvim",         -- required
-          "sindrets/diffview.nvim",        -- optional - Diff integration
-          -- Only one of these is needed.
-          "nvim-telescope/telescope.nvim", -- optional
-        --   "ibhagwan/fzf-lua",              -- optional
-        --   "echasnovski/mini.pick",         -- optional
-        },
-        config = true,
-        cmd = "Neogit"
-      }
+ "NeogitOrg/neogit",
+ cmd = "Neogit",
+ config = function()
+  require("neogit").setup({
+   kind = "split", -- opens neogit in a split 
+   signs = {
+    -- { CLOSED, OPENED }
+    section = { "", "" },
+    item = { "", "" },
+    hunk = { "", "" },
+   },
+   integrations = { diffview = true }, -- adds integration with diffview.nvim
+  })
+ end,
 }
