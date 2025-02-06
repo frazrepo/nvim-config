4a7781e618f48b19ef3c6cf6e1a1d4accd487739
frazrepo <frazvisuals@gmail.com>
2025-02-06 12:52:13 +0100

    Fix bufferline lazy loading

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -44,6 +44,7 @@ return {
     -- bufferline
     {
         'akinsho/bufferline.nvim',
+        event = "VeryLazy",
         dependencies = {
             {'kyazdani42/nvim-web-devicons'}
         },
