766dfe51bbe321d0710f28f38afdc487f00f66d9
frazrepo <frazvisuals@gmail.com>
2025-02-09 17:56:35 +0100

    Remove disabled plugins configuration and cursorline setup from UI enhancements

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -2,25 +2,6 @@
 -- UI enhacements
 -----------------------------------------------------------
 return {
-    -- cursorline
-    {
-        'yamatsum/nvim-cursorline',
-        config = function ()
-            require('nvim-cursorline').setup {
-                cursorline = {
-                    enable = true,
-                    timeout = 1000,
-                    number = false,
-                },
-                cursorword = {
-                    enable = true,
-                    min_length = 3,
-                    hl = { underline = true },
-                }
-            }
-        end
-    },
-
     -- lualine
     {
         'nvim-lualine/lualine.nvim',
