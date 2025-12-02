5779025c55292fc14546fbce4d7e3cf49bc3e068
frazrepo <frazvisuals@gmail.com>
2025-12-02 21:35:10 +0100

    Enhance completion configuration: enable cmdline completion, add event triggers, and refine options

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -2,20 +2,23 @@ return {
     -----------------------------------------------------------
     -- Completion blink
     -----------------------------------------------------------
-	{ -- Optional
+	{
 		"saghen/blink.cmp",
-		event = { "InsertEnter" },
+		event = { "InsertEnter", "CmdlineEnter" },
 		-- optional: provides snippets for the snippet source
 		dependencies = {
-			 "rafamadriz/friendly-snippets",
+			"rafamadriz/friendly-snippets",
 		},
 		version = "*",
 		opts = {
+			snippets = {
+				preset = "default",
+			},
+
 			-- 'default' for mappings similar to built-in completion
 			-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
 			-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
 			-- See the full "keymap" documentation for information on defining your own keymap.
-			-- keymap = { preset = 'default' },
 			keymap = { preset = "enter" },
 			-- Choosing enter for preset here so we can complete copilot with tab
 
@@ -23,22 +26,51 @@ return {
 				use_nvim_cmp_as_default = false,
 				nerd_font_variant = "mono",
 			},
+
 			sources = {
-				default = { "lsp", "snippets", "buffer"},
-				cmdline = { "cmdline", "path" },
+				 default = { "lsp", "path", "snippets", "buffer" },
 			},
-			-- Enable cmdline completion with command and path sources
-			cmdline = {},
+			cmdline = {
+				enabled = true,
+				keymap = {
+					preset = "cmdline",
+					["<Right>"] = false,
+					["<Left>"] = false,
+				},
+				completion = {
+				list = { selection = { preselect = false } },
+				menu = {
+					auto_show = function(ctx)
+					return vim.fn.getcmdtype() == ":"
+					end,
+				},
+				ghost_text = { enabled = true },
+				},
+			},
+			signature = {
+				enabled = true,
+			},
 			completion = {
+				accept = {
+					-- experimental auto-brackets support
+					auto_brackets = {
+						enabled = true,
+					},
+				},
+				menu = {
+					draw = {
+						treesitter = { "lsp" },
+					},
+				},
 				documentation = {
 					auto_show = true,
 					auto_show_delay_ms = 200,
-				  },
-				trigger = {
-					show_on_insert_on_trigger_character = false,
-				}
-			}
+				},
+				ghost_text = {
+					enabled = true,
+				},
+			},
 		},
-		opts_extend = { "sources.default" }
+		opts_extend = { "sources.default" },
 	},
 }
