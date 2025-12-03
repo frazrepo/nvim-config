15d7cd33d7d56a6048110ccfa8cd64072e0a8c73
frazrepo <frazvisuals@gmail.com>
2025-12-03 21:26:40 +0100

    Remove mini.files

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -109,32 +109,6 @@ return {
             })
         end,
      },
-    -- mini.files
-    -- Usage : navigate using hjkl
-    -- Manipalute file/dir creation like a normal buffer
-    -- confirm operation with =
-    -- Close dialog with q
-    {
-        'nvim-mini/mini.files',
-        version = '*',
-        opts = {},
-        keys = {
-            {
-                "-",
-                function()
-                    require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
-                end,
-                desc = "Open mini.files (Directory of Current File)",
-            },
-            {
-                "<leader>fm",
-                function()
-                    require("mini.files").open(vim.uv.cwd(), true)
-                end,
-                desc = "Open mini.files (cwd)",
-            },
-        },
-    },
     -----------------------------------------------------------
     -- Better Quickfix vim-bqf
     -----------------------------------------------------------
