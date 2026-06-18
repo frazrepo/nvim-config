dd08987910df5d5a21a74cea27f7eae19ca36692
frazrepo <frazvisuals@gmail.com>
2026-06-18 22:56:31 +0200

    Cleaning

--- lua/plugins/extras/init.lua
+++ lua/plugins/extras/init.lua
@@ -3,8 +3,6 @@
 return {
     { import = 'plugins.extras.util'},
     { import = 'plugins.extras.ui'},
-    { import = 'plugins.extras.ai'},
-    { import = 'plugins.extras.coding'},
 
     -- Validated plugins in extras should be move to specific folder
     { import = 'plugins.extras.lab'},
